90a4a13f1d874aa08e2db181f236cd104e7241a7
Antoine Caps <147519666+Aetherum97@users.noreply.github.com>
2025-05-15 16:02:34 +0200

    Chores (Application) :
    Add all Contract's services and interfaces

---
+++ Src/Application/DevLife.Application/Modules/Contracts/Services/ContractService.cs
@@ -0,0 +1,13 @@
+﻿using DevLife.Application.Modules.Contracts.Interfaces.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevLife.Application.Modules.Contracts.Services
+{
+    public class ContractService : IContractService
+    {
+    }
+}
