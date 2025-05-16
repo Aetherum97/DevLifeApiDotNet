1d4f930c90f945edbf026b2cea02dee5372b0e00
Antoine Caps <147519666+Aetherum97@users.noreply.github.com>
2025-05-16 09:41:59 +0200

    Feature (Infrastructure) :
    Add modelBuilder's Material and Contract in AppDbContext

--- Src/Infrastructure/DevLife.Infrastructure/Persistence/Contexts/AppDbContext.cs
+++ Src/Infrastructure/DevLife.Infrastructure/Persistence/Contexts/AppDbContext.cs
@@ -6,7 +6,9 @@ using DevLife.Domain.Modules.Employees;
 using DevLife.Domain.Modules.Materials;
 using DevLife.Infrastructure.Commons.Configurations;
 using DevLife.Infrastructure.Commons.Extensions;
+using DevLife.Infrastructure.Modules.Contracts.Configurations;
 using DevLife.Infrastructure.Modules.Employees.Configurations;
+using DevLife.Infrastructure.Modules.Materials.Configurations;
 using Microsoft.EntityFrameworkCore;
 
 namespace DevLife.Infrastructure.Persistence.Contexts;
@@ -29,6 +31,8 @@ public class AppDbContext(DbContextOptions<AppDbContext> options, IAuthenticated
     // Contracts Module
 
     public DbSet<Contract> Contract { get; set; } = null!;
+    public DbSet<ContractTemplate> ContractTemplate { get; set; } = null!;
+    public DbSet<ContractType> ContractType { get; set; } = null!;
 
     // Companies Module
 
@@ -37,6 +41,8 @@ public class AppDbContext(DbContextOptions<AppDbContext> options, IAuthenticated
     // Materials Module
 
     public DbSet<Material> Material { get; set; } = null!;
+    public DbSet<MaterialSkill> MaterialSkill { get; set; } = null!;
+    public DbSet<MaterialTemplate> MaterialTemplate { get; set; } = null!;
 
 
     public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new())
@@ -63,10 +69,16 @@ public class AppDbContext(DbContextOptions<AppDbContext> options, IAuthenticated
         modelBuilder.ApplyConfiguration(new EmployeeSkillModificatorConfiguration());
 
         // Contracts Module
+        modelBuilder.ApplyConfiguration(new ContractConfiguration());
+        modelBuilder.ApplyConfiguration(new ContractTemplateConfiguration());
+        modelBuilder.ApplyConfiguration(new ContractTypeConfiguration());
 
         // Companies Module
 
         // Materials Module
+        modelBuilder.ApplyConfiguration(new MaterialConfiguration());
+        modelBuilder.ApplyConfiguration(new MaterialSkillConfiguration());
+        modelBuilder.ApplyConfiguration(new MaterialTemplateConfiguration());
 
     }
 }
